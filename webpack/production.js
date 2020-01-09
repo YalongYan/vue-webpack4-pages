2cb26e09b14dc92a0f1b6e08830066d61a686beb
yanyalong <1240277991@qq.com>
2020-01-09 20:19:37 +0800

    cleanWebpackPlugin

--- webpack/production.js
+++ webpack/production.js
@@ -6,7 +6,8 @@ const base = require('./base')
 const modules = require('../src/modules')
 
 const TerserJSPlugin = require('terser-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+// const CleanWebpackPlugin = require('clean-webpack-plugin') // 这个写法不可以
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
@@ -91,11 +92,11 @@ module.exports = merge.smart(base, {
     //     ignore: ['.*']
     //   }]
     // ),
-    // new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
       chunkFilename: '[id].[hash].css',
     }), 
-    new BundleAnalyzerPlugin()
+    // new BundleAnalyzerPlugin()
   ]
 })
